3968a25f3854900336d73cb8067b7546b27e8316
hrapov-v <hrapov-v@bk.ru>
2021-06-19 12:23:26 +0300

    видимость кнопок по айди добавил

--- app/src/main/java/superky/keytwo/less2hw/MainActivity.java
+++ app/src/main/java/superky/keytwo/less2hw/MainActivity.java
@@ -3,6 +3,8 @@ package superky.keytwo.less2hw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText editText findViewById(R.id.editText);
+        Button b1 = findViewById(R.id.b1);
+        Button b2 = findViewById(R.id.b2);
+        Button b3 = findViewById(R.id.b3);
+        Button b4 = findViewById(R.id.b4);
+        Button b5 = findViewById(R.id.b5);
+        Button b6 = findViewById(R.id.b6);
+        Button b7 = findViewById(R.id.b7);
+        Button b8 = findViewById(R.id.b8);
+        Button b9 = findViewById(R.id.b9);
+        Button b0 = findViewById(R.id.b0);
+        Button equal = findViewById(R.id.equal);
+        Button clear = findViewById(R.id.bClear);
+        Button div = findViewById(R.id.bDiv);
+        Button sub = findViewById(R.id.bSub);
+        Button mul = findViewById(R.id.bMul);
+        Button sum = findViewById(R.id.bSum);
+        Button dot = findViewById(R.id.dot);
     }
 }
